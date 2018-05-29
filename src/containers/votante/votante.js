5e3d9af234af3a1029f311b614c4529a9fc1a190
Johnny Pacheco <johnnye96pv@gmail.com>
2018-05-28 23:28:45 -0500

    Ya registra votos

--- src/containers/votante/votante.js
+++ src/containers/votante/votante.js
@@ -6,6 +6,7 @@ import { withRouter } from 'react-router';
 import { logout, setCurrentUser } from '../../redux/actions/login';
 import {Tabs, Tab} from 'react-bootstrap-tabs';
 import config from '../../config/default.json';
+import SweetAlert from 'sweetalert-react'; // eslint-disable-line import/no-extraneous-dependencies
 import './tabs.css';
 import './candidatos.css';
 
@@ -19,9 +20,59 @@ class Votante extends React.Component{
             candidatos3 : [],
             organo1: '',
             organo2: '',
-            organo3: ''
+            organo3: '',
+            alert: false,
+            msgAlert: "",
+            keyTab: 0,
+            votado: false
         }
         this.handleOptionChange = this.handleOptionChange.bind(this);
+        this.votar = this.votar.bind(this);
+        this.handleSelectTab = this.handleSelectTab.bind(this);
+    }
+
+    votar(){
+        if(this.state.organo1 === '' || this.state.organo2 === '' || this.state.organo3 === ''){
+            this.setState({error: true})
+        }else{
+            graphqlClient(`
+                mutation{
+                    organo1: newVoto(votoNew:{
+                        candidatoId: ${this.state.organo1}
+                        mesaId: ${this.props.user.mesaId}
+                    }){
+                        id
+                    }
+                    organo2: newVoto(votoNew:{
+                        candidatoId: ${this.state.organo2}
+                        mesaId: ${this.props.user.mesaId}
+                    }){
+                        id
+                    }
+                    organo3: newVoto(votoNew:{
+                        candidatoId: ${this.state.organo3}
+                        mesaId: ${this.props.user.mesaId}
+                    }){
+                        id
+                    }
+                }
+            `)
+            .then( ({data}) => {
+                if(data.error){
+                    alert(data.error.msg);
+                }else{
+                    this.setState({
+                        alert: true,
+                        msgAlert: "Voto registrado",
+                        votado: true
+                    });
+
+                }
+            })
+            .catch( err => {
+                alert(err);
+            });
+        }
     }
 
     componentWillMount(){
@@ -32,8 +83,13 @@ class Votante extends React.Component{
         this.setState({
           [changeEvent.target.name]: changeEvent.target.value
         });
-      }
+    }
 
+    handleSelectTab(key) {
+        console.log('selected' + key);
+        this.setState({keyTab: key});
+    }
+    
     render(){
         return(
             <div id="admin-container" className="conte">
@@ -50,8 +106,12 @@ class Votante extends React.Component{
                     </div>
                 </header>
                 <section className="section-tables" >
-                    <Tabs style={{border:'2px solid green'}} headerStyle={{fontWeight: 'bold'}} activeHeaderStyle={{color:'blue'}} contentStyle={{backgroundColor:'lightgoldenrodyellow'}}>
-                        <Tab label="Consejo Superior" >
+                    <Tabs
+                        activeKey={this.state.keyTab}
+                        selected={this.state.keyTab}
+                        onSelect={this.handleSelectTab}
+                        >
+                        <Tab eventKey={0} label="Consejo Superior" >
                             <form className="containers-candidatos" >
                                 {
                                     this.state.candidatos1 && this.state.candidatos1.map( (cur, key) => {
@@ -62,8 +122,8 @@ class Votante extends React.Component{
                                                 <input
                                                     type="radio"
                                                     name="organo1"
-                                                    value={cur.nombre}
-                                                    checked={this.state.organo1 === cur.nombre}
+                                                    value={cur.id}
+                                                    checked={this.state.organo1 == cur.id}
                                                     onChange={this.handleOptionChange} 
                                                 />
                                             </div>
@@ -72,7 +132,7 @@ class Votante extends React.Component{
                                 }
                             </form>
                         </Tab>
-                        <Tab label="Consejo Academico" >
+                        <Tab eventKey={1} label="Consejo Academico" >
                             <form className="containers-candidatos" >
                                 {
                                     this.state.candidatos2 && this.state.candidatos2.map( (cur, key) => {
@@ -83,8 +143,8 @@ class Votante extends React.Component{
                                                 <input
                                                     type="radio"
                                                     name="organo2"
-                                                    value={cur.nombre}
-                                                    checked={this.state.organo2 === cur.nombre}
+                                                    value={cur.id}
+                                                    checked={this.state.organo2 == cur.id}
                                                     onChange={this.handleOptionChange} 
                                                 />
                                             </div>
@@ -93,7 +153,7 @@ class Votante extends React.Component{
                                 }
                             </form>
                         </Tab>
-                        <Tab label="CIARP" >
+                        <Tab eventKey={2} label="CIARP" >
                             <form className="containers-candidatos" >
                                 {
                                     this.state.candidatos3 && this.state.candidatos3.map( (cur, key) => {
@@ -104,8 +164,8 @@ class Votante extends React.Component{
                                                 <input
                                                     type="radio"
                                                     name="organo3"
-                                                    value={cur.nombre}
-                                                    checked={this.state.organo3 === cur.nombre}
+                                                    value={cur.id}
+                                                    checked={this.state.organo3 == cur.id}
                                                     onChange={this.handleOptionChange} 
                                                 />
                                             </div>
@@ -124,8 +184,27 @@ class Votante extends React.Component{
                             this.props.history.push('/');
                         }
                     } />
+                    <input type="button" defaultValue="Votar" onClick={this.votar} />
                     <img className="pie" src={require('../../img/pie.png')} />
                 </footer>
+                <SweetAlert
+                    show={this.state.alert}
+                    title="Notificacion"
+                    text={this.state.msgAlert}
+                    onConfirm={() => {
+                        this.setState({
+                            alert: null
+                        },() => {
+                            if(this.state.votado){
+                                logout();
+                                this.props.setCurrentUser({});
+                                this.props.history.push('/');
+                            }
+                        });
+                    }}
+                    onEscapeKey={() => this.setState({ alert: false })}
+                    onOutsideClick={() => this.setState({ alert: false })}
+                />
             </div>
         );
     }
